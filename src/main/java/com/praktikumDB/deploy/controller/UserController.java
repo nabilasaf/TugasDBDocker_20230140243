96e21179ef3aa12e1669df08b814ee4f9d31c424
nabilasaf <nabilasafitri861@gmail.com>
2026-04-21 14:40:03 +0700

    menambahkan isi dari user controller

--- src/main/java/com/praktikumDB/deploy/controller/UserController.java
+++ src/main/java/com/praktikumDB/deploy/controller/UserController.java
@@ -1,9 +1,41 @@
 package com.praktikumDB.deploy.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.praktikumDB.deploy.model.User;
+import com.praktikumDB.deploy.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String creatUser(@RequestBody User request){
+        userService.addUser(request);
+        return "User created successfully";
+    }
+    @GetMapping
+    public List<User> getAllUser(){
+        return userService.getAllUsers();
+    }
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable String id){
+        return userService.getUserById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id){
+        userService.deleteUser(id);
+        return "User deleted successfully";
+    }
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody User request){
+        userService.updateUser(id, request);
+        return "User updated successfully";
+    }
 }
