2c8944c02eaa9b8b356d0f9dcaf32bf602a48bd3
nabilasaf <nabilasafitri861@gmail.com>
2026-04-21 14:12:58 +0700

    menambahkan user service

---
+++ src/main/java/com/praktikumDB/deploy/service/UserService.java
@@ -0,0 +1,9 @@
+package com.praktikumDB.deploy.service;
+
+import com.praktikumDB.deploy.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+}
